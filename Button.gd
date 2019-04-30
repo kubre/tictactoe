04fa2f16219fb9c1b58e1742e6bd88ff3422def1
vkubre <v@kubre.in>
2019-04-30 10:25:05 +0530

    Wrote some code

--- Button.gd
+++ Button.gd
@@ -1,4 +1,25 @@
 extends TextureButton
 
-export (int) var number
+const xmark = preload("res://Assets/xmark.png")
+const omark = preload("res://Assets/omark.png")
 
+var value: int
+export (int) var row = -1
+export (int) var col = -1
+
+signal im_pressed(button)
+
+func _ready() -> void:
+    reset()
+
+func set_x(val = 1) -> void:
+    value = val
+
+func set_o(val = 10) -> void:
+    value = val
+
+func reset() -> void:
+    value = 0
+
+func _on_Button_Pressed() -> void:
+    emit_signal("im_pressed", self)
--- World.gd
+++ World.gd
@@ -1,13 +1,19 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+var board = [ 
+                [ $TextureButton, $TextureButton2, $TextureButton3],
+                [ $TextureButton4, $TextureButton5, $TextureButton6],
+                [ $TextureButton7, $TextureButton8, $TextureButton9]
+            ]
 
-# Called when the node enters the scene tree for the first time.
+const X = 1
+const O = 10
+
+var active_player : = X
+var current_round : = 0
+var winner : = false
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+    for row in board:
+        for btn in row:
+            btn.connect("im_pressed", self, "onButtonPressed")
