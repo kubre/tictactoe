f414f03114116762e9058f4e5e99346e8c5b7c30
vkubre <v@kubre.in>
2019-05-01 08:59:15 +0530

    Can played as dual players and checks for draws

--- TTTButton.gd
+++ TTTButton.gd
@@ -1,4 +1,6 @@
+# TTTButton        
 extends TextureButton
+class_name TTTButton
 
 const xmark = preload("res://Assets/xmark.png")
 const omark = preload("res://Assets/omark.png")
@@ -14,12 +16,15 @@ func _ready() -> void:
 
 func set_x(val = 1) -> void:
     value = val
+    texture_normal = xmark
 
 func set_o(val = 10) -> void:
     value = val
+    texture_normal = omark
 
 func reset() -> void:
     value = 0
+    texture_normal = null
 
 func _on_Button_Pressed() -> void:
     emit_signal("im_pressed", self)
--- World.gd
+++ World.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var board = [ 
-                [ $TextureButton, $TextureButton2, $TextureButton3],
-                [ $TextureButton4, $TextureButton5, $TextureButton6],
-                [ $TextureButton7, $TextureButton8, $TextureButton9]
+onready var board = [ 
+                [ $TTTButton1, $TTTButton2, $TTTButton3],
+                [ $TTTButton4, $TTTButton5, $TTTButton6],
+                [ $TTTButton7, $TTTButton8, $TTTButton9]
             ]
 
 const X = 1
@@ -14,6 +14,45 @@ var current_round : = 0
 var winner : = false
 
 func _ready() -> void:
+    # Attaches onButtonPressed to all buttons on board
     for row in board:
         for btn in row:
             btn.connect("im_pressed", self, "onButtonPressed")
+  
+func onButtonPressed(btn: TTTButton) -> void:
+    # Handles entire round from marking place to checking win condition
+    place_mark(btn, active_player)
+
+    # Increase Round    
+    current_round += 1
+    
+    # If all places are marked then declare draw and reset the game 
+    if current_round == 9:
+        msgbox("Draw", "Draw game! both played well, Click ok to reset game", "reset")
+    
+func place_mark(btn: TTTButton, player: int) -> void:
+    # Actullay places marks board
+    if player == X:
+        btn.set_x(X)
+        active_player = O
+    else:
+        btn.set_o(O)
+        active_player = X
+        
+    print(btn.row, btn.col, btn.value)
+ 
+func reset() -> void:
+    # Resets the Game
+    active_player = X
+    current_round = 0
+    for row in board:
+        for btn in row:
+            btn.reset()
+ 
+
+func msgbox(title: String, message: String, ok_method: String) -> void:
+    # This is method just helps to display Message Box
+    $Dialog.window_title = title
+    $Dialog.dialog_text = message
+    $Dialog.connect("confirmed", self, ok_method)
+    $Dialog.popup_centered_minsize()
